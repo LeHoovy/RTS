f8752a658320608680dbcc838156834219bfee46
Hoovy <107582136+LeHoovy@users.noreply.github.com>
2023-03-13 16:23:42 -0600

    Updated unit movement and helped with clutter
    Right click to angle unit

--- GAME/Units/UnitMove.gd
+++ GAME/Units/UnitMove.gd
@@ -1,16 +1,17 @@
-extends Node3D
+extends CharacterBody3D
 
 var rayOrigin = Vector3()
 var rayTarget = Vector3()
+
 
 func _process(delta):
 	
 	var mousePos = get_viewport().get_mouse_position()
 #	print("Mouse Position: ", mousePos)
 	
-	var rayOrigin = $Camera3D.project_ray_origin(mousePos)
+	var rayOrigin = get_parent().get_node("Camera3D").project_ray_origin(mousePos)
 #	print("rayOrigin: ", rayOrigin)
-	rayTarget = rayOrigin + $Camera3D.project_ray_normal(mousePos) * 2000
+	rayTarget = rayOrigin + get_parent().get_node("Camera3D").project_ray_normal(mousePos) * 2000
 	
 	var spaceState = get_world_3d().direct_space_state
 	var parameters = PhysicsRayQueryParameters3D.create(rayOrigin, rayTarget)
@@ -18,7 +19,7 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("MB2"):
 		if not intersect.is_empty():
-			print("NOT EMPTY!")
+#			print("NOT EMPTY!")
 			var pos = intersect.position
-			var lookAtTarget = Vector3(pos.x, $Unit.position.y, pos.z)
-			$Unit.look_at(lookAtTarget, Vector3.UP )
+			var lookAtTarget = Vector3(pos.x, position.y, pos.z)
+			look_at(lookAtTarget, Vector3.UP )
