3b2dff00174a2a6970a8eda1b355fc9e0542828a
Hoovy <107582136+LeHoovy@users.noreply.github.com>
2023-03-22 19:02:04 -0600

    beginning of movement/pathfinding
    bit buggy rn tho

--- GAME/Units/UnitMove.gd
+++ GAME/Units/UnitMove.gd
@@ -1,30 +1,62 @@
-extends CharacterBody3D
+extends CharacterBody3D           
 
 var rayOrigin = Vector3()
 var rayTarget = Vector3()
 
+var speed = 50.0
+
+@onready var navAgent = $NavigationAgent3D
+
+var path = []
+
+func _physics_process(delta):
+	var current_location = global_transform.origin
+	var next_location = navAgent.get_next_path_position()
+	var newVelocity =  (next_location - current_location).normalized() * speed * delta
+	
+	velocity = newVelocity
+	move_and_slide()
+	
+	
+#func updateTargetLocation(targetLocation):
+#	navAgent.set_target_location(targetLocation)
+	
 
 func _process(delta):
-	
+
 	var mousePos = get_viewport().get_mouse_position()
-#	print("Mouse Position: ", mousePos)
-	
+	#print("Mouse Position: ", mousePos)
+
 	rayOrigin = get_parent().get_node("Camera3D").project_ray_origin(mousePos)
-#	print("rayOrigin: ", rayOrigin)
+	#print("rayOrigin: ", rayOrigin)
 	rayTarget = rayOrigin + get_parent().get_node("Camera3D").project_ray_normal(mousePos) * 2000
 	#Locate mouse position and set position for a ray to the world
-	
+
 	var spaceState = get_world_3d().direct_space_state
 	var parameters = PhysicsRayQueryParameters3D.create(rayOrigin, rayTarget)
 	var intersect = spaceState.intersect_ray(parameters)
 	#Locate x,z position for intercept with terrain
-	
+
 	if Input.is_action_just_pressed("MB2"):
 		if not intersect.is_empty():
-#			print("NOT EMPTY!")
+			#print("NOT EMPTY!")
 			var pos = intersect.position
 			var lookAtTarget = Vector3(pos.x, position.y, pos.z)
 			look_at(lookAtTarget, Vector3.UP )
+			
+			navAgent.set_target_position(pos)
 			#Point at location when mouse button 2 pressed
-	
-	
+#
+#			navAgent.set_target_position(lookAtTarget)
+#			var current_location = global_transform.origin
+#			var next_location = navAgent.get_next_path_position()
+#			var newVelocity =  (next_location - current_location).normalized() * speed
+#
+#			velocity = newVelocity
+#			while get_parent().global_transform.origin != next_location:
+#				print("MOVING")
+#
+#				move_and_slide()
+
+
+
