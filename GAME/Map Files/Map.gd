db7f4a6508e91265f6500f51a5bab31c6784f574
Hoovy <107582136+LeHoovy@users.noreply.github.com>
2023-11-13 15:40:32 -0700

    Just finishing pushing my changes
    e

--- GAME/Map Files/Map.gd
+++ GAME/Map Files/Map.gd
@@ -26,8 +26,8 @@ func bake():
 func bake_navmesh(affected: Array[Node]) -> void:
 	for cell in get_used_cells():
 		pass
-	print(get_cell_neighbors(Vector3i(0, 1, 0)))
-	print(get_cell_neighbors(Vector3i(0, 0, 0)).size())
+	print(get_all_neighbors(Vector3i(0, 1, 0)))
+	print(get_all_neighbors(Vector3i(0, 0, 0)).size())
 	print()
 	print(get_vertical_neighbors(Vector3i(0, 0, 0,)))
 	print(get_vertical_neighbors(Vector3i(0,0,0)).size())
@@ -36,18 +36,29 @@ func bake_navmesh(affected: Array[Node]) -> void:
 
 func get_vertical_neighbors(cell: Vector3i) -> Array[Vector3i]:
 	var neighbors: Array[Vector3i]
-	neighbors.append_array(get_cell_neighbors(cell))
+	neighbors.append_array(get_all_neighbors(cell))
 	neighbors.append(Vector3i(cell.x, cell.y + 1, cell.z))
-	neighbors.append_array(get_cell_neighbors(Vector3i(cell.x, cell.y - 1, cell.z)))
+	neighbors.append_array(get_all_neighbors(Vector3i(cell.x, cell.y - 1, cell.z)))
 	neighbors.append(Vector3i(cell.x, cell.y + 1, cell.z))
-	neighbors.append_array(get_cell_neighbors(Vector3i(cell.x, cell.y + 1, cell.z)))
+	neighbors.append_array(get_all_neighbors(Vector3i(cell.x, cell.y + 1, cell.z)))
 	return neighbors
 
 
-func get_cell_neighbors(cell: Vector3i) -> Array[Vector3i]:
+func get_all_neighbors(cell: Vector3i) -> Array[Vector3i]:
 	var neighbors: Array[Vector3i]
 	for x in range(-1, 2):
 		for y in range(-1, 2):
 			if Vector2i(x, y) != Vector2i(0, 0):
 				neighbors.append(Vector3i(cell.x + x, cell.y, cell.y + y))
+	return neighbors
+
+
+func get_cell_neighbors(cell: Vector3i) -> Array[Vector3i]:
+	var neighbors: Array[Vector3i] = [
+		Vector3i(cell.x + 1 , cell.y, cell.z),
+		Vector3i(cell.x, cell.y, cell.z + 1),
+		Vector3i(cell.x - 1, cell.y, cell.z),
+		Vector3i(cell.x, cell.y, cell.z - 1)
+	]
+	
 	return neighbors
