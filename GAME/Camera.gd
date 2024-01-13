b347e03b4e6a4fd53eac9e7689ab68bf085161b3
Hoovy <107582136+LeHoovy@users.noreply.github.com>
2024-01-12 22:31:40 -0700

    just a bit of updating y'know

--- GAME/Camera.gd
+++ GAME/Camera.gd
@@ -1,13 +1,16 @@
 extends Node3D
 class_name Camera
 
-
 @export_category("Sensitivity")
-@export_range(0.1, 20) var edge_scroll_sens: float = 3
+@export_range(1, 100) var edge_scroll_sens: int = 10
+@export var smooth_cam_move: bool = false
+@export_range(0.01, 3) var smooth_cam_time: float = 0.5
+@export var drag_sens: float = 4
+@export_range(0.1, 10, 0.1) var move_margin: float = 1
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
@@ -27,17 +30,27 @@ func is_mouse_on_edge(percentage: float) -> Vector2i:
 	#print(mouse_pos.y <= (percentage / 100) * screen_size.y)
 	return output
 
-
+var not_delta: Vector2
 var friction: float = 0.9
 var move: Vector2i
-func process_move(delta) -> void:
+func process_move(delta: float) -> void:
 	mouse_pos = get_viewport().get_mouse_position()
 	screen_size = get_viewport().get_visible_rect().size
-	
-	move = is_mouse_on_edge(1)
-	cam_move_speed += move * (0.3 * (edge_scroll_sens / 3)) 
+	move = is_mouse_on_edge(move_margin)
+	if smooth_cam_move:
+		calc_move(delta)
+	else:
+		cam_move_speed = move * edge_scroll_sens
+	position.x += cam_move_speed.x * delta
+	position.z += cam_move_speed.y * delta
+
+func calc_move(delta: float) -> void:
+	#mouse_pos = get_viewport().get_mouse_position()
+	#screen_size = get_viewport().get_visible_rect().size
+	#move = is_mouse_on_edge(1)
+	cam_move_speed += move * (edge_scroll_sens * delta * 2)
 	#cam_move_speed *= friction
-	cam_move_speed = clamp(cam_move_speed, Vector2(-edge_scroll_sens / 3, -edge_scroll_sens / 3), Vector2(edge_scroll_sens / 3, edge_scroll_sens / 3))
+	cam_move_speed = cam_move_speed.clamp(Vector2(-edge_scroll_sens, -edge_scroll_sens), Vector2(edge_scroll_sens, edge_scroll_sens))
 	
 	if move.x == 0:
 		cam_move_speed.x = 0
@@ -53,7 +66,7 @@ func process_move(delta) -> void:
 var mouse_pos: Vector2
 var screen_size: Vector2
 var cam_move_speed: Vector2
-func _process(delta):
+func _process(delta: float) -> void:
 	process_move(delta)
 
 
@@ -62,10 +75,10 @@ var zoomMin: float = 0.3
 var zoomMax: float = 5.0
 var zoom: float = 1
 #Sets the minimum and maximum zoom, as well as the speed of zooming.
-@export var dragSens: float = 4
+
 #Changes the sensitivity of the ability to drag around the camera.
 
-func _input(event):
+func _input(event: Variant) -> void:
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
 		position.x -= event.relative.x / 49.5
 		position.z -= event.relative.y / 50
@@ -80,3 +93,21 @@ func _input(event):
 			zoom -= zoomSpeed
 		#Changes zoom as long, and only if, the mouse wheel is being scrolled
 		zoom = clamp(zoom, zoomMin, zoomMax)
+
+#const ray_length: int = 2000
+#func _physics_process(delta):
+	#var physic_proc_mouse_pos = get_viewport().get_mouse_position()
+	#var from: Vector3 = $Camera3D.project_ray_origin(physic_proc_mouse_pos)
+	#var to: Vector3 = from + $Camera3D.project_ray_normal(physic_proc_mouse_pos) * ray_length
+	#var space: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
+	#var ray_query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.new()
+	#ray_query.from = from
+	#ray_query.to = to
+	#ray_query.collide_with_areas = true
+	#var raycast_result: Dictionary = space.intersect_ray(ray_query)
+	#
+	#if not raycast_result.is_empty():
+		#print("Not empty")
+		#var pos: Vector3 = raycast_result.position
+		#var look_at_me: Vector3 = Vector3(pos.x, $Player.position.y, pos.z)
+		#$Player.look_at(look_at_me, Vector3.UP)
