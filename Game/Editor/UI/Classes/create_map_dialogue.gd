4c555667ee0ac3a7bdeada04e497c36bc5c077a1
.Avi <107582136+dot-Avi@users.noreply.github.com>
2025-10-29 08:21:38 -0600

    Revert "added more setup to create_map_dialogue"
    This reverts commit 6bd78937534fa04a7dab8d79a24bcad85cc31251.

--- Game/Editor/UI/Classes/create_map_dialogue.gd
+++ Game/Editor/UI/Classes/create_map_dialogue.gd
@@ -4,10 +4,6 @@ extends Panel
 
 
 var _parent_window: Window
-var _new_map_x_input: OptionButton
-var _new_map_y_input: OptionButton
-var _create_button: Button
-var _cancel_button: Button
 
 
 func _ready() -> void:
@@ -16,23 +12,6 @@ func _ready() -> void:
 		queue_free()
 	_parent_window = get_parent()
 
-	_new_map_y_input = (
-			get_node("VBoxContainer/Y Axis/Options/HBoxContainer/Control3/Options")
-	) as OptionButton
-	_new_map_x_input = (
-			get_node("VBoxContainer/X Axis/Options/HBoxContainer/Control3/Options")
-	) as OptionButton
-
-	_create_button = get_node("VBoxContainer/Confirmation/Create") as Button
-	_cancel_button = get_node("VBoxContainer/Confirmation/Cancel") as Button
-
-	_cancel_button.pressed.connect(_on_cancel_pressed)
-	_create_button.pressed.connect(_on_create_pressed)
-
 
 func _on_cancel_pressed() -> void:
 	_parent_window.queue_free()
-
-
-func _on_create_pressed() -> void:
-	pass
