319ec09a7a66514acd72d3040d9aaa6db6d9ee40
.Avi <107582136+dot-Avi@users.noreply.github.com>
2025-10-29 09:18:23 -0600

    update editor and new map dialog

--- Game/Editor/Classes/map_editor.gd
+++ Game/Editor/Classes/map_editor.gd
@@ -10,7 +10,8 @@ extends Node3D
 # Chunks should probably have their own classes
 # If chunks are their own classes, instances, or even scenes, might not need this to extend Node3D
 
-@export_tool_button("Generate debug map", "Callable") var gen_map : Callable = _debug_gen_new_map_window
+@export_tool_button("Create new map", "Callable") var gen_map : Callable = _new_map
+@export var window_size := Vector2(500, 720)
 
 const CREATE_MAP_DIALOGUE = preload("uid://bko3lfheh3efu")
 
@@ -24,22 +25,28 @@ var _map_corners: Dictionary[Vector3, Array]
 var chunks: Array[Node3D]
 var _map: Map
 
-func _debug_gen_new_map_window() -> void:
+
+# Open the new map creation dialog when generate map is pressed
+func _new_map() -> void:
 	# position_finder is generated at the same size as the new editor window at the center
 	# its position is then taken for the new editor window and used
 	var position_finder := Window.new()
 	position_finder.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_OTHER_SCREEN
-	position_finder.size = Vector2(500, 720)
+	position_finder.size = window_size
 	add_child(position_finder)
 	
 	# Generate the new map window prompt and create it in the editor
 	# Free the position finder's memory afterwards
 	var new_map_window := Window.new()
-	EditorInterface.popup_dialog(new_map_window, Rect2i(position_finder.position, Vector2(500, 720)))
+	EditorInterface.popup_dialog(new_map_window, Rect2i(position_finder.position, window_size))
 	position_finder.queue_free()
 	
 	new_map_window.close_requested.connect(func() -> void:
 		new_map_window.queue_free()
-	)
+	) # Close window on cancel code block
 	
 	new_map_window.add_child(CREATE_MAP_DIALOGUE.instantiate())
+
+
+func generate_new_map(map_size: Vector2) -> void:
+	pass
--- Game/Editor/UI/Classes/create_map_dialog.gd
+++ Game/Editor/UI/Classes/create_map_dialog.gd
@@ -1,20 +1,63 @@
 @tool
-class_name NewMapDialog
 extends Panel
 
 
+# Parent window and map editor vars for storage
 var _parent_window: Window
+var _map_editor: MapEditor
+
+var _new_map_x_input: OptionButton
+var _new_map_y_input: OptionButton
+var _cancel_button: Button
+var _create_button: Button
 
 
 func _ready() -> void:
+	# First make sure this isn't opened in the editor
+	# If it isn't, and it can't find the MapEditor node, free its memory
+	# If it also isn't opened in a new window, free its memory
 	if self == get_tree().edited_scene_root:
 		print("Scene opened in editor, scene is not new window, ignoring")
 		return
+	if not get_tree().edited_scene_root.has_node("MapEditor"):
+		print("Not opened from map editor scene, freeing memory")
+		queue_free()
 	if get_parent() is not Window:
 		print("Parent isn't a window, scene is not open in editor, freeing memory")
 		queue_free()
+	
+	# Finally, set your parent window variable to your parent window
+	# And map editor to map editor node
 	_parent_window = get_parent()
+	_map_editor = get_tree().edited_scene_root.get_node("MapEditor") as MapEditor
+	
+	# Set up option input variables
+	_new_map_y_input = (
+			get_node("VBoxContainer/Y Axis/Options/HBoxContainer/Control3/Options")
+	) as OptionButton
+	_new_map_x_input = (
+			get_node("VBoxContainer/X Axis/Options/HBoxContainer/Control3/Options")
+	) as OptionButton
+	
+	# Set up and connect signals of confirmation buttons
+	_create_button = get_node("VBoxContainer/Confirmation/Create") as Button
+	_cancel_button = get_node("VBoxContainer/Confirmation/Cancel") as Button
+	_cancel_button.pressed.connect(_on_cancel_pressed)
+	_create_button.pressed.connect(_on_create_pressed)
 
 
+# When operation is cancelled, delete the window
 func _on_cancel_pressed() -> void:
+	_parent_window.queue_free()
+
+
+# When operation is confirmed, send the output to the map editor node
+func _on_create_pressed() -> void:
+	var new_map_size := Vector2(
+			int(_new_map_x_input.get_item_text(_new_map_x_input.selected)),
+			int(_new_map_y_input.get_item_text(_new_map_y_input.selected)),
+	)
+	
+	_map_editor.generate_new_map(new_map_size)
+	await get_tree().process_frame
 	_parent_window.queue_free()
