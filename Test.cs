236c914c33cf4f2bb3728e17ee5ff540c653b689
dot-Avi <hiddenflashbacks@gmail.com>
2026-03-25 20:34:36 -0600

    More steam testing stuff

--- Test.cs
+++ Test.cs
@@ -1,33 +1,54 @@
 using Godot;
+using Godot.Collections;
 using Games.Indiegesindel;
 using System;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 public partial class Test : Node3D
 {
-	private Steam _steamInstance;
+	private Steam _instance;
 
 	// Called when the node enters the scene tree for the first time.
-	public override async void _Ready()
+	public override void _Ready()
 	{
-		_steamInstance = Steam.GetSingleton();
+		Node testSpace = GetNode<Node3D>("%test3d");
+		GD.Print(testSpace.Name);
 
-		if (_steamInstance == null)
+		_instance = Steam.GetSingleton();
+
+		if (_instance == null)
 		{
 			GD.PrintErr("Steam is not running! Quitting!");
 			//GetTree().Quit();
 			return;
 		}
 
-		_steamInstance.SteamInit(480, false);
+		Dictionary initStatus = _instance.SteamInitEx(0, true);
+		if ((int)initStatus["status"] == 0)
+		{
+			GD.Print("Steam has correctly initialized.");
+		} else if ((int)initStatus["status"] == 2)
+		{
+			GD.PrintErr("Cannot connect to steam! Steam likely is not running.");
+		} else if ((int)initStatus["status"] == 3)
+		{
+			GD.PrintErr("Steam client is out of date! Please update.");
+		} else if ((int)initStatus["status"] == 1)
+		{
+			GD.PrintErr("Steam has failed to initialize! Quitting!");
+			//GetTree().Quit();
+		}
+		
 		/*GD.Print("Steam is running! You are:");
 		//	ulong userID = Steam.GetSteamID();
-		GD.Print(_steamInstance.GetPersonaName());*/
+		GD.Print(_instance.GetPersonaName());*/
 
-		var name = _steamInstance.GetPersonaName();
-		var state = _steamInstance.GetPersonaState();
+		var name = _instance.GetPersonaName();
+		var state = _instance.GetPersonaState();
 		GD.Print($"Persona: {name} ({state})");
-		GD.Print($"Account level: {_steamInstance.GetPlayerSteamLevel()}");
+		GD.Print($"Account level: {_instance.GetPlayerSteamLevel()}");
+		GD.Print($"Game id: {_instance.CurrentAppId}");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
