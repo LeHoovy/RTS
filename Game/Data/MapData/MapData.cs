bdf774c425ee7790c39e888f12a69d457b05ad05
dot-Avi <hiddenflashbacks@gmail.com>
2026-03-30 15:19:34 -0600

    I don't remember what I updated

--- Game/Data/MapData/MapData.cs
+++ Game/Data/MapData/MapData.cs
@@ -1,7 +1,25 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
+using Range = Godot.Range;
 
 public partial class MapData : Resource
 {
 	public byte[] heightmap;
+
+
+	public static MapData NewMap(Vector2I mapSize, byte initialDepth)
+	{
+		MapData newMap = new MapData();
+
+		for (byte x = 0; x < mapSize.X; x++)
+		{
+			for (byte y = 0; y < mapSize.Y; y++)
+			{
+				
+			}
+		}
+
+		return newMap;
+	}
 }
