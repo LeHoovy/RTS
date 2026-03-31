3a04cfb35d6f9f87a998735a5ac052b6606f9fee
.Avi <107582136+dot-Avi@users.noreply.github.com>
2026-03-31 09:13:57 -0600

    Refactor MapData heightmap/pos logic and update Test
    Initialize HeightMap in MapData with correct size and refactor position conversion APIs: add GetChunkAtPos overloads, rename TileToHeightMapPos/HeightMapPosToTile to ConvertPos and fix X/Y scaling math and bounds checks; streamline heightmap generation logic and add debug/error warnings. Update Test.cs to rename Steam field, increase generated map to 16x16, fix Image.SetData dimensions to match the heightmap, save debug PNG, and use the renamed Steam instance with early returns on init errors. Also include updated test/map.png binary.

--- Game/Data/MapData/MapData.cs
+++ Game/Data/MapData/MapData.cs
@@ -18,10 +18,13 @@ public partial class MapData : Resource
 	/// <returns></returns>
 	public static MapData NewMap(Vector2I mapSize, byte initialDepth)
 	{
-		MapData newMap = new MapData();
-		newMap.MapSize = mapSize * 16;
+		MapData newMap = new MapData
+		{
+			MapSize = mapSize * 16,
+			HeightMap = new byte[mapSize.X * 16 * mapSize.Y * 16]
+		};
 
-		newMap.HeightMap = new byte[newMap.MapSize.X * newMap.MapSize.Y];
+		//newMap.HeightMap = new byte[newMap.MapSize.X * newMap.MapSize.Y];
 		GD.Print(newMap.HeightMap.Length);
 		int iteration = 0;
 		for (byte y = 0; y < newMap.MapSize.Y; y++)
@@ -31,21 +34,25 @@ public partial class MapData : Resource
 				if (iteration > newMap.MapSize.X * newMap.MapSize.Y)
 				{
 					GD.PrintErr($"Iteration ({iteration}) is too high!");
+					GD.PushError($"Iteration ({iteration}) is too high!");
 					return newMap;
 				}
 
+				// DEBUG MAP GEN
 				// Get chunk pos
-				Vector2I chunkPos = new Vector2I((int)Math.Floor(x / 16.0), (int)Math.Floor(y / 16.0));
-				GD.Print($"Tile: ({x}, {y}) in chunk: {chunkPos}");
-				int newHeight = (int)(Math.Floor(x / 16.0) + y);
-				/*if (x > y)
+				Vector2I chunkPos = newMap.GetChunkAtPos(new Vector2I(x, y));
+				int newHeight = (int)(chunkPos.X * 2 + chunkPos.Y * 2);
+				if (x % 16 > y % 16)
 				{
 					newHeight = (byte)(newHeight + 1);
-				}*/
+				}
 
-				newMap.HeightMap[newMap.TileToHeightMapPos(new Vector2I(x, y))] = (byte)(newHeight * 32 % 255);//(byte)(initialDepth * 8);
-				
+				newMap.HeightMap[newMap.ConvertPos(new Vector2I(x, y))] = (byte)(newHeight * 16 % 255);
+				// Final. Use later and comment out/delete the previous code as it will be unneeded
+				//newMap.HeightMap[newMap.ConvertPos(new Vector2I(x, y))] = (byte)(initialDepth * 8);
+
 				iteration++;
+
 				if (x == 255)
 				{
 					break;
@@ -62,43 +69,83 @@ public partial class MapData : Resource
 
 
 	/// <summary>
-	/// Takes a specific map tile and returns what position in the HeightMap that tile's data would be found.
+	/// Finds a chunk containing a given tile.
 	/// </summary>
-	/// <param name="tile">Vector2I containing the location of the tile that we want to get the HeightMap position of. Must be on the map.</param>
-	/// <returns>If the tile's location exists on the map, returns what position in the HeightMap it is found. If not, returns 0.</returns>
-	private ushort TileToHeightMapPos(Vector2I tile)
+	/// <param name="pos"></param>
+	/// <returns>Chunk position that contains the given position.</returns>
+	public Vector2I GetChunkAtPos(Vector2I pos)
 	{
-		if (tile.X < 0 || tile.X >= MapSize.X)
+		if (pos.X < 0 || pos.X >= MapSize.X)
 		{
-			GD.PrintErr($"Could not find a tile at position ({tile.X}, {tile.Y})!");
-			GD.PushError($"Could not find a tile at position ({tile.X}, {tile.Y})!");
-			return 0;
-		}
-		if (tile.Y < 0 || tile.Y >= MapSize.Y)
+			GD.PrintErr("Warning: Position not on map! Returned chunk will not exist!");
+			GD.PushWarning("Warning: Position not on map! Returned chunk will not exist!");
+		} else if (pos.Y < 0 || pos.Y >= MapSize.Y)
 		{
-			GD.PrintErr($"Could not find a tile at position ({tile.X}, {tile.Y})!");
-			GD.PushError($"Could not find a tile at position ({tile.X}, {tile.Y})!");
-			return 0;
+			GD.PrintErr("Warning: Position not on map! Returned chunk will not exist!");
+			GD.PushWarning("Warning: Position not on map! Returned chunk will not exist!");
 		}
 
-		return (ushort)(tile.X + (tile.Y * 16));
+		Vector2I chunkPos = new Vector2I(0, 0);
+		chunkPos.X = (int)Math.Floor(pos.X / 16.0);
+		chunkPos.Y = (int)Math.Floor(pos.Y / 16.0);
+		return chunkPos;
 	}
 
 
 	/// <summary>
-	/// Takes a position in the heightmap and converts it to a tile position on the map.
+	/// Overload for GetChunkAtPos() that takes a heightmap position.<br/>
+	/// Finds a chunk at a given position.
 	/// </summary>
-	/// <param name="mapPos">A position on the heightmap.</param>
-	/// <returns>(-1, -1) if "mapPos" is not on the heightmap. Otherwise returns the tile position of the heightmap position.</returns>
-	private Vector2I HeightMapPosToTile(ushort mapPos)
+	/// <param name="pos">A position on the heightmap.</param>
+	/// <returns>Chunk position that contains the given position.</returns>
+	public Vector2I GetChunkAtPos(ushort pos)
 	{
-		if (mapPos > HeightMap.Length)
+		return GetChunkAtPos(ConvertPos(pos));
+	}
+
+
+	/// <summary>
+	/// Converts a tile position into a heightmap position.<br/>
+	/// Vice Versa for overload.
+	/// </summary>
+	/// <param name="pos">Tile position we want to find the heightmap position of.</param>
+	/// <returns>Heightmap position of the given tile.
+	/// If the tile is not on the map, returns 0.</returns>
+	private ushort ConvertPos(Vector2I pos)
+	{
+		if (pos.X < 0 || pos.X >= MapSize.X)
 		{
-			GD.PrintErr($"Position ''{mapPos}'' is out of range. Max range: {HeightMap.Length}");
-			GD.PushError($"Position ''{mapPos}'' is out of range. Max range: {HeightMap.Length}");
+			GD.PrintErr($"Could not find a tile at position ({pos.X}, {pos.Y})!");
+			GD.PushError($"Could not find a tile at position ({pos.X}, {pos.Y})!");
+			return 0;
+		}
+		if (pos.Y < 0 || pos.Y >= MapSize.Y)
+		{
+			GD.PrintErr($"Could not find a tile at position ({pos.X}, {pos.Y})!");
+			GD.PushError($"Could not find a tile at position ({pos.X}, {pos.Y})!");
+			return 0;
+		}
+
+		return (ushort)(pos.X + (pos.Y * MapSize.X));
+	}
+
+
+	/// <summary>
+	/// Converts a heightmap position to a map position.<br/>
+	/// Vice Versa for overload.
+	/// </summary>
+	/// <param name="pos">Position on the heightmap to convert to a map position.</param>
+	/// <returns>Returns the tile position of the given heightmap position.
+	/// Returns the (-1, -1) if the given position does not exist..</returns>
+	private Vector2I ConvertPos(ushort pos)
+	{
+		if (pos > HeightMap.Length)
+		{
+			GD.PrintErr($"Position ''{pos}'' is out of range. Max range: {HeightMap.Length}");
+			GD.PushError($"Position ''{pos}'' is out of range. Max range: {HeightMap.Length}");
 			return new Vector2I(-1, -1);
 		}
 
-		return new Vector2I(mapPos % 16, (int)Math.Floor((double)mapPos / 16));
+		return new Vector2I(pos % MapSize.X, (int)Math.Floor((double)pos / MapSize.X));
 	}
 }
--- Test.cs
+++ Test.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 
 public partial class Test : Node3D
 {
-	private Steam _instance;
+	private Steam instance;
 	private MapData testMapData;
 
 	// Called when the node enters the scene tree for the first time.
@@ -15,51 +15,54 @@ public partial class Test : Node3D
 	{
 		// Non-Steam stuff
 		ulong microStart = Time.GetTicksUsec();
-		testMapData = MapData.NewMap(new Vector2I(2, 16), 1);
+		testMapData = MapData.NewMap(new Vector2I(16, 16), 1);
 		ulong microEnd = Time.GetTicksUsec();
 		GD.Print($"Time elapsed:\n{microEnd-microStart} microseconds\n{Math.Round((microEnd-microStart) / 100.0) / 10} milliseconds");
 		GD.Print();
 
+		// debug heightmap creation
 		Image img = new Image();
-		img.SetData(32, 256, false, Image.Format.R8, testMapData.HeightMap);
+		img.SetData(256, 256, false, Image.Format.R8, testMapData.HeightMap);
 		img.SavePng("res://test/map.png");
 		Sprite2D newSprite = new Sprite2D();
 		GetNode<Sprite2D>("%map").Texture = ImageTexture.CreateFromImage(img);
 
 		// Steam Stuff
-		_instance = Steam.GetSingleton();
-		if (_instance == null)
+		instance = Steam.GetSingleton();
+		if (instance == null)
 		{
 			GD.PrintErr("Steam is not running! Quitting!");
 			//GetTree().Quit();
 			return;
 		}
 
-		Dictionary initStatus = _instance.SteamInitEx(0, true);
+		Dictionary initStatus = instance.SteamInitEx(0, true);
 		if ((int)initStatus["status"] == 0)
 		{
 			GD.Print("Steam has correctly initialized.");
 		} else if ((int)initStatus["status"] == 2)
 		{
 			GD.PrintErr("Cannot connect to steam! Steam likely is not running.");
+			return;
 		} else if ((int)initStatus["status"] == 3)
 		{
 			GD.PrintErr("Steam client is out of date! Please update.");
 		} else if ((int)initStatus["status"] == 1)
 		{
-			GD.PrintErr("Steam has failed to initialize! Quitting!");
+			//GD.PrintErr("Steam has failed to initialize! Quitting!");
 			//GetTree().Quit();
+			return;
 		}
 		
 		/*GD.Print("Steam is running! You are:");
 		//	ulong userID = Steam.GetSteamID();
-		GD.Print(_instance.GetPersonaName());*/
+		GD.Print(instance.GetPersonaName());*/
 
-		var name = _instance.GetPersonaName();
-		var state = _instance.GetPersonaState();
+		var name = instance.GetPersonaName();
+		var state = instance.GetPersonaState();
 		GD.Print($"Persona: {name} ({state})");
-		GD.Print($"Account level: {_instance.GetPlayerSteamLevel()}");
-		GD.Print($"Game id: {_instance.CurrentAppId}");
+		GD.Print($"Account level: {instance.GetPlayerSteamLevel()}");
+		GD.Print($"Game id: {instance.CurrentAppId}");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
